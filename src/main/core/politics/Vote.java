3c01255c5d0507e460e9055c0178e50808ced02c
stevenlagoy <stevenlagoy@gmail.com>
2025-10-22 00:03:45 -0400

    Add date and time fields, and flags for early and mail-in voting to Vote

--- src/main/core/politics/Vote.java
+++ src/main/core/politics/Vote.java
@@ -7,27 +7,112 @@
 
 package main.core.politics;
 
-import main.core.characters.Character;
-
 import java.time.LocalDate;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Collection;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import main.core.characters.Character;
+import main.core.map.MapEntity;
 
 public class Vote {
     
-    private int turnout;
+    private boolean mailInVotingAllowed;
+    private boolean earlyVotingAllowed;
+    private LocalTime pollsOpenTime;
+    private LocalTime pollsCloseTime;
+    private LocalDateTime earlyDateTime;
+    private LocalDateTime closeDateTime;
+    private LocalDateTime mailInCloseDateTime;
+    private LocalDateTime overseasCloseDateTime;
+    private MapEntity location;
     private Map<Character, Integer> recipients;
-    private LocalDate date;
+    private int turnout;
 
     public Vote(Collection<Character> recipients, LocalDate date) {
         this.turnout = 0;
         this.recipients = recipients.stream().collect(Collectors.toMap(k->k, v->0, (a, b) -> a, HashMap::new)); // Turn collection into a map of Character to 0.
-        this.date = date;
+        this.earlyDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+        this.closeDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+        this.overseasCloseDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+        this.mailInVotingAllowed = false;
+        this.earlyVotingAllowed = false;
+    }
+
+    public boolean isMailInVotingAllowed() {
+        return mailInVotingAllowed;
+    }
+    public void setMailInVotingAllowed(boolean allowed) {
+        this.mailInVotingAllowed = allowed;
+    }
+
+    public LocalTime getPollsOpenTime() {
+        return pollsOpenTime;
+    }
+    public void setPollsOpenTime(LocalTime time) {
+        this.pollsOpenTime = time;
+    }
+
+    public LocalTime getPollsCloseTime() {
+        return pollsCloseTime;
+    }
+    public void setPollsCloseTime(LocalTime time) {
+        this.pollsCloseTime = time;
+    }
+
+    public boolean isEarlyVotingAllowed() {
+        return earlyVotingAllowed;
+    }
+    public void setEarlyVotingAllowed(boolean allowed) {
+        this.earlyVotingAllowed = allowed;
+    }
+
+    public LocalDateTime getDate() {
+        return closeDateTime;
+    }
+    public void setDate(LocalDate date) {
+        this.earlyDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+        this.closeDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+        this.overseasCloseDateTime = LocalDateTime.of(date, LocalTime.of(23, 59));
+    }
+
+    public LocalDateTime getEarlyDateTime() {
+        return earlyDateTime;
+    }
+    public void setEarlyDateTime(LocalDateTime dateTime) {
+        this.earlyDateTime = dateTime;
+    }
+
+    public LocalDateTime getCloseDateTime() {
+        return closeDateTime;
+    }
+    public void setCloseDateTime(LocalDateTime dateTime) {
+        this.closeDateTime = dateTime;
+    }
+
+    public LocalDateTime getMailInCloseDateTime() {
+        return mailInCloseDateTime;
+    }
+    public void setMailInCloseDateTime(LocalDateTime dateTime) {
+        this.mailInCloseDateTime = dateTime;
+    }
+
+    public LocalDateTime getOverseasCloseDateTime() {
+        return overseasCloseDateTime;
+    }
+    public void setOverseasCloseDateTime(LocalDateTime dateTime) {
+        this.overseasCloseDateTime = dateTime;
+    }
+
+    public MapEntity getLocation() {
+        return location;
+    }
+    public void setLocation(MapEntity location) {
+        this.location = location;
     }
 
     public int getTurnout() {
