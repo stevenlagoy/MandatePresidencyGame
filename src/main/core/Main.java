60733bde60fe1ec75f6f87f153bab5186adbbd03
stevenlagoy <stevenlagoy@gmail.com>
2025-12-07 04:02:07 -0500

    Add some more names sorted by race / ethnicity and decade

--- src/main/core/Main.java
+++ src/main/core/Main.java
@@ -5,6 +5,8 @@
 
 package main.core;
 
+import main.core.characters.Character;
+
 public class Main {
 
     /** This class is non-instantiable. The {@code main()} entry point should be accessed in a static way. */
@@ -16,11 +18,20 @@ public class Main {
 
     public static void main(String[] args) {
         int errorCode = 0;
+        
+        // Initialize the engine
         engine = new Engine();
         if (!engine.init()) return;
 
+        // Create some characters
+        for (int i = 0; i < 100; i++) {
+            Character c = new Character();
+            IOUtil.stdout.println(c.getName().getBiographicalName());
+        }
+
         engine.writeGameState();
 
+        // Clean up the engine
         engine.cleanup();
         Logger.log("Main Done");
         System.exit(errorCode);
