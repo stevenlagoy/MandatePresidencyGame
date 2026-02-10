50193fb18e061a98b83e95b23a7cc64ee2c6b08b
stevenlagoy <stevenlagoy@gmail.com>
2026-02-09 19:10:49 -0500

    Basic 3D test

---
+++ core/src/main/java/com/stevenlagoy/presidency/Basic3DTest.java
@@ -0,0 +1,82 @@
+package com.stevenlagoy.presidency;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Basic3DTest implements ApplicationListener {
+
+    public PerspectiveCamera cam;
+    public Model model;
+    public ModelBatch modelBatch;
+    public ModelInstance instance;
+    public Environment environment;
+    public CameraInputController camController;
+
+    @Override
+    public void create() {
+
+        modelBatch = new ModelBatch();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);
+    }
+
+    @Override
+    public void resize(int width,int height) {
+    }
+
+    @Override
+    public void render() {
+
+        ScreenUtils.clear(0, 0, 0, 1, true);
+
+        camController.update();
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        modelBatch.dispose();
+        model.dispose();
+    }
+    
+}
--- lwjgl3/src/main/java/com/stevenlagoy/presidency/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/stevenlagoy/presidency/lwjgl3/Lwjgl3Launcher.java
@@ -2,6 +2,7 @@ package com.stevenlagoy.presidency.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.stevenlagoy.presidency.Basic3DTest;
 import com.stevenlagoy.presidency.Main;
 
 /** Launches the desktop (LWJGL3) application. */
@@ -12,7 +13,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Basic3DTest(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
