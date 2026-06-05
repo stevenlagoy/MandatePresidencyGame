67201527ae9257c7be415252b5817c012e6a3b45
stevenlagoy <stevenlagoy@gmail.com>
2026-06-05 02:12:57 -0400

    Allow citizen attributes for names and families to be copied to aid with citizen building flows. Also add properties for family to get some extended members without a lot of calls (IE siblings, cousins, grandparents).

--- core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/Family.kt
+++ core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/Family.kt
@@ -3,17 +3,69 @@ package com.stevenlagoy.presidency.characters.attributes
 import com.stevenlagoy.jsonic.JSONObject
 import com.stevenlagoy.jsonic.Jsonic
 import com.stevenlagoy.presidency.characters.Citizen
+import com.stevenlagoy.presidency.core.Engine
+import kotlin.uuid.ExperimentalUuidApi
 
+@OptIn(ExperimentalUuidApi::class)
 data class Family(
-    val parents: MutableSet<Citizen>,
-    var spouse: Citizen?,
-    val children: MutableSet<Citizen>,
+    val ENGINE: Engine,
+    var mother: Citizen? = null,
+    var father: Citizen? = null,
+    var spouse: Citizen? = null,
+    val children: MutableSet<Citizen> = mutableSetOf(),
 ) : Jsonic<Family> {
-    override fun fromJson(json: JSONObject): Family {
-        return this
+
+    init {
+        assert(mother?.sex == Sex.FEMALE || mother?.sex == Sex.INTERSEX) { "Mother's sex must be female or intersex" }
+        assert(father?.sex == Sex.MALE || father?.sex == Sex.INTERSEX) { "Father's sex must be male or intersex" }
     }
 
-    override fun toJson(): JSONObject {
-        return JSONObject()
+    val parents = setOf(mother, father).filterNotNull()
+
+    val grandparents: Set<Citizen> get() = parents.flatMap { it.family.parents }.toSet()
+
+    val siblings: Set<Citizen> get() = parents.flatMap { it.family.children }.toSet()
+
+    val piblings: Set<Citizen> get() = parents.flatMap { it.family.siblings }.toSet()
+
+    val cousins: Set<Citizen> get() = piblings.flatMap { it.family.children }.toSet()
+
+    val niblings: Set<Citizen> get() = cousins.flatMap { it.family.children }.toSet()
+
+    val grandchildren: Set<Citizen> get() = children.flatMap { it.family.children }.toSet()
+
+    fun copy(other: Family) {
+        this.mother = other.mother
+        this.father = other.father
+        this.spouse = other.spouse
+        this.children.apply {
+            clear()
+            addAll(other.children)
+        }
     }
+
+    override fun fromJson(json: JSONObject) = this
+
+    override fun toJson() = JSONObject(hashCode().toString(), listOf(
+        JSONObject("parents", parents.map { it.id }),
+        JSONObject("spouse", spouse?.id),
+        JSONObject("children", children.map { it.id }),
+    ))
+
+    fun getSideSize(): Int {
+        return (mother?.family?.getSideSize() ?: 0) + (father?.family?.getSideSize() ?: 0) + (spouse?.family?.getSideSize() ?: 0) + children.sumOf { it.family.getSideSize() }
+    }
+
+    fun getFamilyPlan()= FamilyManager.FamilyPlan(
+        mother != null,
+        mother?.family?.getSideSize() ?: 0,
+        father != null,
+        father?.family?.getSideSize() ?: 0,
+        siblings.size,
+        siblings.sumOf { it.family.getSideSize() },
+        spouse != null,
+        spouse?.family?.getSideSize() ?: 0,
+        children.size,
+        children.sumOf { it.family.getSideSize() },
+    )
 }
