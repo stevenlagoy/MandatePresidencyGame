4d52ad862e69bab6537789cbc0c1c33d9d0205b2
stevenlagoy <stevenlagoy@gmail.com>
2026-06-06 15:35:09 -0400

    Add starting experience classes.

--- core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/experiences/BusinessExperience.kt
+++ core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/experiences/BusinessExperience.kt
@@ -1,12 +1,15 @@
 package com.stevenlagoy.presidency.characters.attributes.experiences
 
+import com.stevenlagoy.presidency.map.Municipality
 import java.time.LocalDate
 
 class BusinessExperience(
+    name: String,
     startDate: LocalDate,
     endDate: LocalDate?,
     description: String,
+    location: Municipality,
     val companyName: String,
     val title: String,
-) : Experience(startDate, endDate, description) {
+) : Experience(name, startDate, endDate, description, location) {
 }
--- core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/experiences/LegislativeExperience.kt
+++ core/src/main/kotlin/com/stevenlagoy/presidency/characters/attributes/experiences/LegislativeExperience.kt
@@ -1,12 +1,15 @@
 package com.stevenlagoy.presidency.characters.attributes.experiences
 
+import com.stevenlagoy.presidency.map.Municipality
 import com.stevenlagoy.presidency.politics.Chamber
 import java.time.LocalDate
 
 class LegislativeExperience(
+    name: String,
     startDate: LocalDate,
     endDate: LocalDate?,
     description: String,
+    location: Municipality,
     chamber: Chamber,
-) : Experience(startDate, endDate, description) {
+) : Experience(name, startDate, endDate, description, location) {
 }
