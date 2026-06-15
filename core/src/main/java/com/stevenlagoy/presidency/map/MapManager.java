b681af3ae0c22687a69cf156bea1b46b27641d7b
stevenlagoy <stevenlagoy@gmail.com>
2026-06-15 02:18:15 -0400

    Create methods in MapManager to gather JSON files for MapEntities and to build entities from that data. Remove old system.

--- core/src/main/java/com/stevenlagoy/presidency/map/MapManager.java
+++ core/src/main/java/com/stevenlagoy/presidency/map/MapManager.java
@@ -8,15 +8,14 @@ import com.stevenlagoy.presidency.core.Manager;
 import com.stevenlagoy.presidency.demographics.Demographics;
 import com.stevenlagoy.presidency.map.travel.route.RouteManager;
 import com.stevenlagoy.presidency.util.*;
-import net.mgsx.gltf.scene3d.model.CubicVector3;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * <h1>MAP MANAGER</h1>
@@ -24,7 +23,7 @@ import java.util.*;
  * <p>
  *     <b>Author:  </b> Steven LaGoy     <br>
  *     <b>Created: </b> 10 December 2024 <br>
- *     <b>Modified:</b> 02 June 2026     <br>
+ *     <b>Modified:</b> 15 June 2026     <br>
  * </p>
  *
  * MapManager is responsible for creating and tracking information about {@link MapEntity} instances,
@@ -68,6 +67,15 @@ public class MapManager extends Manager {
     private final @NotNull Set<Municipality> municipalities;
 
     private final @NotNull Set<University> universities;
+    private final @NotNull Set<Descriptor> descriptors;
+
+    private final @NotNull Set<CensusRegion> censusRegions;
+    private final @NotNull Set<CensusDivision> censusDivisions;
+
+    private MapIndex mapIndex;
+    public MapIndex getMapIndex() {
+        return mapIndex;
+    }
 
     // Constructors
 
@@ -79,6 +87,9 @@ public class MapManager extends Manager {
         counties = new HashSet<>();
         municipalities = new HashSet<>();
         universities = new HashSet<>();
+        descriptors = new HashSet<>();
+        censusRegions = new HashSet<>();
+        censusDivisions = new HashSet<>();
     }
 
     // Manager Methods
@@ -91,7 +102,23 @@ public class MapManager extends Manager {
 
     @Override
     protected void doInit() {
-        readNationData();
+        Set<Set<Path>> statesPaths = getStatesPaths();
+        Set<Path> statesDataPaths = getStatesDataPaths(statesPaths);
+        Set<Path> statesMunicipalitiesPaths = getMunicipalitiesPaths(statesPaths);
+        Set<Path> countiesPaths = getCountiesPaths(statesPaths);
+        Set<Path> congressionalDistrictPaths = getCongressionalDistrictPaths(statesPaths);
+        Path nationPath = FilePaths.NATION;
+        Path censusRegionsDivisionsPath = FilePaths.CENSUS_REGIONS_DIVISISONS;
+
+        createMunicipalities(statesMunicipalitiesPaths);
+        createCounties(countiesPaths);
+        resolveMunicipalitiesCounties();
+        createStates(statesDataPaths);
+        resolveCountiesStates();
+        createNation(nationPath);
+        createCongressionalDistricts(congressionalDistrictPaths);
+        createCensusRegionsDivisions(censusRegionsDivisionsPath);
+        resolveStatesDivisions();
     }
 
     @Override
@@ -118,6 +145,172 @@ public class MapManager extends Manager {
 
     // Instance Methods
 
+    public void loadMapBinaries() {
+        File binFile = Gdx.files.internal("maps/counties.bin").file();
+        MapIndex mapIndex = MapIndex.Companion.load(binFile);
+        int matched = 0, unmatched = 0;
+        for (County county : counties) {
+            if (county.getColor() == null) continue;
+            int countyColor = county.getColor();
+            RegionData region = mapIndex.regionByColor(countyColor);
+            if (region != null) {
+                county.setRegion$core(region);
+                matched++;
+            }
+            else {
+                Logger.error("No map region for %s (color #%s)", county.getFullName(), ColorUtils.toHex(countyColor));
+            }
+        }
+    }
+
+    private void readDescriptors() {
+        JSONObject descriptorsJson = JSONProcessor.processJson(FilePaths.DESCRIPTORS);
+        for (Object descriptorObj : descriptorsJson.getAsList()) {
+            if (descriptorObj instanceof JSONObject descriptorJson) {
+                descriptors.add(new Descriptor(engine, descriptorJson));
+            }
+        }
+    }
+
+    private @NotNull Set<Set<Path>> getStatesPaths() {
+        requireState(ManagerState.INITIALIZING, ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED);
+        try {
+            Set<Path> statePaths = IOUtils.listDirectories(FilePaths.STATES_DIR);
+            Set<Set<Path>> statesPaths = new HashSet<>();
+            for (Path statePath : statePaths) {
+                try {
+                    statesPaths.add(IOUtils.listFiles(statePath));
+                }
+                catch (IOException e) {
+                    onDegraded(e);
+                }
+            }
+            return statesPaths;
+        }
+        catch (IOException e) {
+            onError(e);
+            return new HashSet<>();
+        }
+    }
+
+    private @NotNull Optional<Path> getStateDataPath(@NotNull Set<Path> statePaths) {
+        requireState(ManagerState.INITIALIZING, ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED);
+        return statePaths.stream().filter(path -> path.getFileName().toString().matches("^[0-9]{2}\\.json$")).findFirst();
+    }
+
+    private @NotNull Optional<Path> getStateMunicipalitiesPath(@NotNull Set<Path> statePaths) {
+        requireState(ManagerState.INITIALIZING, ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED);
+        return statePaths.stream().filter(path -> path.getFileName().toString().matches("^[0-9]{2}_municipalities\\.json$")).findFirst();
+    }
+
+    private @NotNull Set<Path> getStateCountyPaths(@NotNull Set<Path> statePaths) {
+        requireState(ManagerState.INITIALIZING, ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED);
+        return statePaths.stream().filter(path -> path.getFileName().toString().matches("^[0-9]{5}\\\\.json$")).collect(Collectors.toSet());
+    }
+
+    private @NotNull Set<Path> getStateCongressionalDistrictPaths(@NotNull Set<Path> statePaths) {
+        requireState(ManagerState.INITIALIZING, ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED);
+        return statePaths.stream().filter(path -> path.getFileName().toString().matches("^[A-Z]{2}-[0-9]+\\.json$")).collect(Collectors.toSet());
+    }
+
+    private @NotNull Set<Path> getStatesDataPaths(@NotNull Set<Set<Path>> statesPaths) {
+        return statesPaths.stream().map(this::getStateDataPath).filter(Optional::isPresent).map(Optional::get).collect(Collectors.toSet());
+    }
+
+    private @NotNull Set<Path> getMunicipalitiesPaths(@NotNull Set<Set<Path>> statesPaths) {
+        return statesPaths.stream().map(this::getStateMunicipalitiesPath).filter(Optional::isPresent).map(Optional::get).collect(Collectors.toSet());
+    }
+
+    private @NotNull Set<Path> getCountiesPaths(@NotNull Set<Set<Path>> statesPaths) {
+        return statesPaths.stream().map(this::getStateCountyPaths).flatMap(Collection::stream).collect(Collectors.toSet());
+    }
+
+    private @NotNull Set<Path> getCongressionalDistrictPaths(@NotNull Set<Set<Path>> statesPaths) {
+        return statesPaths.stream().map(this::getStateCongressionalDistrictPaths).flatMap(Collection::stream).collect(Collectors.toSet());
+    }
+
+    private final Map<String, String> municipalitiesCounties = new HashMap<>();
+    private void createMunicipalities(@NotNull Set<Path> municipalitiesDataPaths) {
+        municipalitiesCounties.clear();
+        for (Path municipalityDataPath : municipalitiesDataPaths) {
+            JSONObject municipalityJson = JSONProcessor.processJson(municipalityDataPath);
+            municipalities.add(new Municipality(engine, municipalityJson));
+            municipalitiesCounties.put(municipalityJson.get("fullName", String.class), municipalityJson.get("county", String.class));
+        }
+    }
+
+    private final Map<String, String> countiesStates = new HashMap<>();
+    private void createCounties(@NotNull Set<Path> countyDataPaths) {
+        for (Path countyDataPath : countyDataPaths) {
+            JSONObject countyJson = JSONProcessor.processJson(countyDataPath);
+            counties.add(new County(engine, countyJson));
+            countiesStates.put(countyJson.get("fullName", String.class), countyJson.get("state", String.class));
+        }
+    }
+
+    private void resolveMunicipalitiesCounties() {
+        for (Map.Entry<String, String> entry : municipalitiesCounties.entrySet()) {
+            Municipality municipality = matchMunicipality(entry.getKey()).orElseThrow();
+            County county = matchCounty(entry.getValue()).orElseThrow();
+            municipality.setCounty$core(county);
+            county.getMunicipalities().add(municipality);
+        }
+    }
+
+    private final Map<String, String> statesDivisions = new HashMap<>();
+    private void createStates(@NotNull Set<Path> statesDataPaths) {
+        for (Path statesDataPath : statesDataPaths) {
+            JSONObject stateJson = JSONProcessor.processJson(statesDataPath);
+            states.add(new State(engine, stateJson));
+            statesDivisions.put(stateJson.get("fullName", String.class), stateJson.get("censusDivision", String.class));
+        }
+    }
+
+    private void resolveCountiesStates() {
+        for (Map.Entry<String, String> entry : countiesStates.entrySet()) {
+            County county = matchCounty(entry.getKey()).orElseThrow();
+            State state = matchState(entry.getValue()).orElseThrow();
+            county.setState$core(state);
+            state.getCounties().add(county);
+        }
+    }
+
+    private void createNation(@NotNull Path nationDataPath) {
+        Nation.INSTANCE.setStates$core(states);
+    }
+
+    private void createCongressionalDistricts(@NotNull Set<Path> congressionalDistrictsDataPaths) {
+        for (Path congressionalDistrictDataPath : congressionalDistrictsDataPaths) {
+            JSONObject districtJson = JSONProcessor.processJson(congressionalDistrictDataPath);
+            congressionalDistricts.add(new CongressionalDistrict(engine, districtJson));
+        }
+    }
+
+    private void createCensusRegionsDivisions(@NotNull Path censusRegionsDivisionsDataPath) {
+        JSONObject censusRegionsDivisionsJson = JSONProcessor.processJson(censusRegionsDivisionsDataPath);
+        for (Object regionObj : censusRegionsDivisionsJson.getAsList()) {
+            if (regionObj instanceof JSONObject regionJson) {
+                censusRegions.add(new CensusRegion(engine, regionJson)); // Also builds Census Divisions
+            }
+        }
+    }
+
+    private void resolveStatesDivisions() {
+        for (Map.Entry<String, String> entry : statesDivisions.entrySet()) {
+            State state = matchState(entry.getValue()).orElseThrow();
+            CensusDivision censusDivision = matchCensusDivision(entry.getKey()).orElseThrow();
+            state.setCensusDivision$core(censusDivision);
+            censusDivision.getStates().add(state);
+        }
+    }
+
+
+
+
+
+
+
+
     public @NotNull Nation getNation() {
         requireOperational();
         return nation;
@@ -143,22 +336,12 @@ public class MapManager extends Manager {
         return municipalities;
     }
 
-    public @NotNull Optional<Municipality> matchMunicipalityByName(@NotNull String name) {
+    public @NotNull Optional<Municipality> matchMunicipality(@NotNull String name) {
         requireState(ManagerState.ACTIVE, ManagerState.PAUSED, ManagerState.DEGRADED, ManagerState.INITIALIZING);
-        Optional<Municipality> res = municipalities.stream().filter(municipality -> municipality.getUniqueName().equals(name)).findFirst();
-        if (res.isEmpty()) res = municipalities.stream().filter(municipality -> municipality.getFullName().equals(name)).findFirst();
-        return res;
-    }
-
-    public @Nullable Municipality getMunicipalityByUniqueName(@NotNull String uniqueName) {
-        requireOperational();
-        return municipalities.stream().filter(municipality -> municipality.getUniqueName().equals(uniqueName)).findFirst().orElse(null);
-    }
-
-    public @Nullable Municipality getMunicipalityByNameAndState(@NotNull String municipalityName, @NotNull State state) {
-        requireOperational();
-        return Objects.requireNonNull(state.getMunicipalities()).stream().filter(municipality -> municipality.getCommonName().equals(municipalityName)).findFirst().orElse(null);
-        // TODO do a better search
+        return Optional.ofNullable(
+            municipalities.stream().filter(municipality -> municipality.getFullName().equals(name)).findFirst().orElse(
+            municipalities.stream().filter(municipality -> municipality.getCommonName().equals(name)).findFirst().orElse(null)
+        ));
     }
 
     public @NotNull Municipality selectMunicipality(Demographics demographics) {
@@ -168,20 +351,9 @@ public class MapManager extends Manager {
         return selected;
     }
 
-    /*
-
-    Nation
-        States
-            Congressional Districts
-            Counties
-            Municipalities
-        Routes
-
-     */
-
     public @NotNull Municipality selectMunicipality() {
         requireOperational();
-        return selectMunicipality(ENGINE.DEMOGRAPHICS_MANAGER.getCommonDemographics());
+        return selectMunicipality(engine.DEMOGRAPHICS_MANAGER.getCommonDemographics());
     }
 
     public @NotNull Municipality getMostPopulatedMunicipality() {
@@ -194,79 +366,31 @@ public class MapManager extends Manager {
         JSONObject json = JSONProcessor.processJson(FilePaths.NATION);
     }
 
-    public void readAllStateData(@NotNull Path stateDataDirectory) {
-        requireState(ManagerState.INITIALIZING);
-        try {
-            Set<Path> dataFiles = IOUtils.listFiles(stateDataDirectory, IOUtils.FileExtension.JSON);
-            State state = null;
-            Set<CongressionalDistrict> congressionalDistricts = new HashSet<>();
-            Set<County> counties = new HashSet<>();
-            Set<Municipality> municipalities;
-            // Find state first
-            for (Path dataFile : dataFiles) {
-                if (dataFile.getFileName().toString().matches("^[0-9]{2}\\.json$")) {
-                    state = new State(ENGINE, JSONProcessor.processJson(dataFile));
-                }
-            }
-            // Check that state was created
-            if (state == null) {
-                Logger.error("NO STATE DATA", String.format("Could not find state data file in directory %s", stateDataDirectory), new Exception());
-                return;
-            }
-            for (Path dataFile : dataFiles) {
-                JSONObject dataJson = JSONProcessor.processJson(dataFile);
-                String fileName = dataFile.getFileName().toString();
-                if (fileName.matches("^[A-Z]{2}-[0-9]+\\.json$")) {
-                    // Congressional District data file
-                    congressionalDistricts.add(new CongressionalDistrict(ENGINE, state, dataJson));
-                } else if (fileName.matches("^[0-9]{5}\\.json$")) {
-                    // County data file
-                    counties.add(new County(ENGINE, state, dataJson));
-                } else if (fileName.matches("^[0-9]{2}_municipalities\\.json$")) {
-                    // Municipalities data file
-                    municipalities = createMunicipalitiesFromData(state, dataJson);
-                } else {
-                    Logger.error("UNKNOWN DATA FILE", String.format("The file %s is not a known datatype", dataFile), new Exception());
-                }
-            }
-            // State data filename matches XX.json
-            // Municipalities data filename matches XX_municipalities.json
-            // County data filenames match XXYYY.json
-            // Congressional District data filenames match AA-N.json
-        } catch (IOException e) {
-            Logger.error(e);
-        }
-    }
-
-    public @NotNull Set<Municipality> createMunicipalitiesFromData(@NotNull State state, @NotNull JSONObject municipalitiesJson) {
-        requireState(ManagerState.INITIALIZING);
-        Set<Municipality> municipalities = new HashSet<>();
-        for (Object municipalityObj : municipalitiesJson.getAsList()) {
-            if (municipalityObj instanceof JSONObject municipalityJson) {
-                municipalities.add(new Municipality(ENGINE, state, municipalityJson));
-            }
-        }
-        return municipalities;
-    }
-
     public @NotNull Optional<University> matchUniversity(@NotNull String name) {
         return universities.stream().filter(university -> university.getCommonName().equals(name)).findFirst();
     }
 
-    public void loadMapBinaries() {
-        File binFile = Gdx.files.internal("maps/counties.bin").file();
-        MapIndex mapIndex = MapIndex.Companion.load(binFile);
-        int matched = 0, unmatched = 0;
-        for (County county : counties) {
-            int countyColor = county.getColor();
-            RegionData region = mapIndex.regionByColor(countyColor);
-            if (region != null) {
-                county.setMapRegion$core(region);
-                matched++;
-            }
-            else {
-                Logger.error("No map region for %s (color #%s)", county.getUniqueName(), ColorUtils.toHex(countyColor));
-            }
-        }
+    public @NotNull Optional<County> matchCounty(String name) {
+        return counties.stream().filter(county -> county.getName().equals(name)).findFirst();
+    }
+
+    public @NotNull Optional<Descriptor> matchDescriptor(String name) {
+        return descriptors.stream().filter(descriptor -> descriptor.getName().equals(name)).findFirst();
+    }
+
+    public @NotNull Optional<State> matchState(String name) {
+        return states.stream().filter(state -> state.getName().equals(name)).findFirst();
+    }
+
+    public @NotNull Optional<CensusRegion> matchCensusRegion(String name) {
+        return censusRegions.stream().filter(censusRegion -> censusRegion.getName().equals(name)).findFirst();
+    }
+
+    public @NotNull Optional<CensusDivision> matchCensusDivision(String name) {
+        return censusDivisions.stream().filter(censusDivision -> censusDivision.getName().equals(name)).findFirst();
+    }
+
+    public @NotNull Set<CensusDivision> getCensusDivisions() {
+        return censusDivisions;
     }
 }
